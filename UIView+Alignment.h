fa309008baec09d992f12385afbef2d58a447cd0
dan <danhassin@mac.com>
2012-02-08 16:47:11 -0500

    init

---
+++ UIView+Alignment.h
@@ -0,0 +1,42 @@
+//
+//  UIView+Alignment.h
+//  PushNews
+//
+//  Created by Dan Hassin on 4/6/10.
+//  Copyright 2010 InContext. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+enum
+{
+	UIViewAlignmentTop					= 1,
+	UIViewAlignmentBottom				= 2,
+	UIViewAlignmentLeft					= 4,
+	UIViewAlignmentRight				= 8,
+	UIViewAlignmentCenteredHorizontal	= 16,
+	UIViewAlignmentCenteredVertical		= 32,
+	UIViewAlignmentCentered				= UIViewAlignmentCenteredHorizontal | UIViewAlignmentCenteredVertical
+}
+typedef UIViewAlignment;
+
+//Masking is possible for UIViewAlignment, for example, you can do this to align to top left of the superview:
+
+// [self alignTo:(UIViewAlignmentTop | UIViewAlignmentLeft)];
+
+
+@interface UIView (alignment)
+
+//by default aligns to the view's superview. does nothing if not in a superview
+- (void) alignTo:(UIViewAlignment)a;
+- (void) alignTo:(UIViewAlignment)a withMargins:(UIEdgeInsets)e;
+
+//aligns frame based on the given view
+- (void) alignTo:(UIViewAlignment)a ofView:(UIView *)v;
+- (void) alignTo:(UIViewAlignment)a withMargins:(UIEdgeInsets)e ofView:(UIView *)view;
+
+//aligns frame based on the given rect
+- (void) alignTo:(UIViewAlignment)a ofRect:(CGRect)r;
+- (void) alignTo:(UIViewAlignment)a withMargins:(UIEdgeInsets)e ofRect:(CGRect)r;
+
+@end
---
+++ UIView+Alignment.m
@@ -0,0 +1,72 @@
+//
+//  UIView+Alignment.m
+//  PushNews
+//
+//  Created by Dan Hassin on 4/6/10.
+//  Copyright 2010 InContext. All rights reserved.
+//
+
+#import "UIView+Alignment.h"
+
+
+@implementation UIView (alignment)
+
+- (void) alignTo:(UIViewAlignment)a withMargins:(UIEdgeInsets)e ofRect:(CGRect)r
+{
+	CGRect rect = self.frame;
+	
+	if (a & UIViewAlignmentCenteredHorizontal)
+		rect.origin.x = (r.size.width-rect.size.width)/2.0;
+	
+	if (a & UIViewAlignmentCenteredVertical)
+		rect.origin.y = (r.size.height-rect.size.height)/2.0;
+	
+	if (a & UIViewAlignmentTop)
+		rect.origin.y = 0;
+	
+	if (a & UIViewAlignmentBottom)
+		rect.origin.y = r.size.height-self.frame.size.height;
+	
+	if (a & UIViewAlignmentLeft)
+		rect.origin.x = 0;
+	
+	if (a & UIViewAlignmentRight)
+		rect.origin.x = r.size.width-self.frame.size.width;
+	
+	rect.origin.x += e.left;
+	rect.origin.x -= e.right;
+	
+	rect.origin.y += e.top;
+	rect.origin.y -= e.bottom;
+	
+	self.frame = rect;
+}
+
+- (void) alignTo:(UIViewAlignment)a withMargins:(UIEdgeInsets)e
+{
+	[self alignTo:a withMargins:e ofView:self.superview];
+}
+
+
+- (void) alignTo:(UIViewAlignment)a
+{
+	[self alignTo:a withMargins:UIEdgeInsetsZero];
+}
+
+- (void) alignTo:(UIViewAlignment)a ofRect:(CGRect)rect
+{
+	[self alignTo:a withMargins:UIEdgeInsetsZero ofRect:rect];	
+}
+
+
+- (void) alignTo:(UIViewAlignment)a ofView:(UIView *)view
+{
+	[self alignTo:a ofRect:view.frame];
+}
+
+- (void) alignTo:(UIViewAlignment)a withMargins:(UIEdgeInsets)e ofView:(UIView *)view
+{
+	[self alignTo:a withMargins:e ofRect:view.frame];
+}
+
+@end
